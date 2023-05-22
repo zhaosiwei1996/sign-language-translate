b6f1208e377bd3ac96b56cbbb91b93b29d6141ad
赵思维 <g21230217b@edu.teu.ac.jp>
2023-05-22 16:58:00 +0900

    开启处理计时

--- bs-demo/server.py
+++ bs-demo/server.py
@@ -47,7 +47,7 @@ socketio.init_app(app, async_mode=None, cors_allowed_origins='*')
 @socketio.on('video_stream')
 def handle_video_stream(image_data):
     # 将Base64编码的图像数据解码为图像
-    # starttime = BaseUtils.get_timestamp()
+    starttime = BaseUtils.get_timestamp()
     encoded_data = image_data.split(',')[1]
     nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
     frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
@@ -68,8 +68,8 @@ def handle_video_stream(image_data):
     # 将绘制了手部动作的图像转换为Base64编码
     ret, buffer = cv2.imencode('.webp', frame)
     encoded_image = base64.b64encode(buffer)
-    # endtime = BaseUtils.get_timestamp()
-    # app.logger.info("ip:%s,流程处理耗时:%s" % (BaseUtils.get_client_ip(), str(endtime - starttime)))
+    endtime = BaseUtils.get_timestamp()
+    app.logger.info("ip:%s,流程处理耗时:%s" % (BaseUtils.get_client_ip(), str(endtime - starttime)))
     # 将结果返回给客户端
     socketio.emit('hand_gesture', encoded_image.decode('utf-8'))
 
