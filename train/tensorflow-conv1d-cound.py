fabf07e2c11ef0a70117e27860d54c0c1bebc680
赵思维 <zhaosiwei2012@gmail.com>
2023-07-13 19:57:10 +0900

    20230713 初步调试通过

--- train/tensorflow-conv1d-cound.py
+++ train/tensorflow-conv1d-cound.py
@@ -1,9 +1,26 @@
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout
 
-input_shape = (271257, 7, 63)
-x = tf.random.normal(input_shape)
-conv1D = tf.keras.layers.Conv1D(10, 10, padding='same')  # 1是输出通道数，3是卷积核大小，不使用边界填充
-max_pool_1d = tf.keras.layers.MaxPooling1D(pool_size=2, strides=1, padding='same')
-y = conv1D(x)
-y = max_pool_1d(y)
-print(y)
+# 假设序列长度为 seq_length
+seq_length = 271257  # 应该根据你的实际数据来设置
+
+# 构建模型
+model = Sequential([
+    Conv1D(32, 3, activation='relu', padding='same', input_shape=(seq_length, 63)),  # 第一层1D卷积层
+    MaxPooling1D(2),  # 第一层1D池化层
+    Dropout(0.5),  # 第一层丢弃层，丢弃率为0.5
+    Conv1D(64, 3, activation='relu', padding='same'),  # 第二层1D卷积层
+    MaxPooling1D(2),  # 第二层1D池化层
+    Dropout(0.5),  # 第二层丢弃层，丢弃率为0.5
+    Flatten(),  # 将卷积层的输出扁平化，以便输入全连接层
+    Dense(128, activation='relu'),  # 全连接层
+    Dropout(0.5),  # 在全连接层后添加一个丢弃层
+    Dense(6, activation='softmax')  # 输出层，假设 num_classes 是你的类别数量
+])
+
+# 编译模型
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# 查看模型结构
+model.summary()
--- train/train-test.py
+++ train/train-test.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding:utf8
-import numpy as np
-import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import pickle
 import datetime
@@ -21,89 +23,71 @@ label_data_dict = {
 for label, arr in label_data_dict.items():
     data.extend(arr)
     labels.extend([label] * len(arr))
-# 转换为 numpy 数组
+
+# 转换为 numpy 数组 数据和标签
 data = np.array(data)
 labels = np.array(labels)
-label_encoder = LabelEncoder()
-encoded_labels = label_encoder.fit_transform(labels)
+
 # 数据集划分为训练集、验证集和测试集
 features_train, features_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.2,
                                                                             random_state=42)
 
-# 构建卷积神经网络模型
-model = tf.keras.Sequential([
-    # 第一层输入层
-    tf.keras.layers.Conv1D(filters=8,  # 卷积层的输出通道数量,根据计算机性能填写
-                           kernel_size=100,  # 卷积核的长度,单数
-                           input_shape=(data.shape[0], data.shape[1]),
-                           # 在tensorflow中,1维卷积操作为从上到下卷积,
-                           # 第一个参数为有x个样本,第二个参数为有多少x列数据
-                           # 在torch中为从左往右卷积
-                           padding='same',  # 自动填充,在torch中需要自己计算
-                           activation='relu'),  # 激活函数
-    # 第一层池化
-    tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='same', input_shape=(data.shape[0], data.shape[1])),
-    # 第一层丢弃
-    tf.keras.layers.Dropout(0.2),
-    # 第二层 输出要减半
-    tf.keras.layers.Conv1D(filters=4,  # 卷积层的输出通道数量,根据计算机性能填写
-                           kernel_size=100,  # 卷积核的长度
-                           input_shape=(data.shape[0], data.shape[1]),
-                           # 在tensorflow中,1维卷积操作为从上到下卷积,
-                           # 第一个参数为有x个样本,第二个参数为有多少x列数据
-                           # 在torch中为从左往右卷积
-                           padding='same',  # 自动填充
-                           activation='relu',  # 激活函数
+# 标签转换
+label_encoder = LabelEncoder()
+encoded_labels = label_encoder.fit_transform(labels_train)
 
-                           ),
-    # 第二层池化
-    tf.keras.layers.MaxPooling1D(pool_size=2, strides=1, padding='same', input_shape=(data.shape[0], data.shape[1])),
-    # 第二层丢弃
-    tf.keras.layers.Dropout(0.2),
-
-    # 全连接层,从卷积层到全连接层的过渡,二维转一维
-    # 输出特征的维度,输入自动判断
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(units=256, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(units=128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(len(label_encoder.classes_), activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-
+model = Sequential([
+    # 第一层卷积
+    Conv1D(256, 100, activation='relu', padding='same', input_shape=(data.shape[1], 1)),  # 第一层1D卷积层
+    MaxPooling1D(2, padding='same', strides=2),  # 第一层1D池化层
+    Dropout(0.2),  # 第一层丢弃层
+    # 第二层卷积
+    Conv1D(128, 100, activation='relu', padding='same', input_shape=(data.shape[1], 1)),  # 第二层1D卷积层
+    MaxPooling1D(1, padding='same', strides=1),  # 第二层1D池化层
+    Dropout(0.2),  # 第二层丢弃层
+    # 全连接层
+    Flatten(),  # 将卷积层的输出扁平化，以便输入全连接层
+    Dense(256, activation='relu'),  # 全连接层
+    Dropout(0.2),  # 丢弃层
+    Dense(128, activation='relu'),  # 全连接层
+    Dropout(0.2),  # 丢弃层
+    Dense(len(label_encoder.classes_), activation='relu'),  # 输出层
+    Dropout(0.2)  # 丢弃层
 ])
+
 # 打印卷积层信息
 model.summary()
 # 编译模型
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 # 开启日志
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-
+#
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 # 训练模型
-history = model.fit(features_train, labels_train, epochs=50, batch_size=64, callbacks=[tensorboard_callback])
+history = model.fit(features_train, encoded_labels, epochs=50, batch_size=2048, callbacks=[tensorboard_callback])
 
-# # 保存模型
-# model.save('./sign-language-model.h5')
+# 保存模型
+model.save('./sign-language-model.h5')
+
 # 验证模型
-# loss, accuracy = model.evaluate(features_test, labels_test)
-# print('Test Accuracy: %f' % (accuracy * 100))
-# # 保存标签编码器
-# with open('sign-language-model.pkl', 'wb') as f:
-#     pickle.dump(encoded_labels, f)
-# #
-# # 绘制时间曲线图
-# plt.plot(history.history['accuracy'], label='Train Accuracy')
-# plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy')
-# plt.legend()
-# plt.show()
-#
-# # 绘制损失函数曲线
-# plt.plot(history.history['loss'], label='Train Loss')
-# plt.plot(history.history['val_loss'], label='Validation Loss')
-# plt.xlabel('Epoch')
-# plt.ylabel('Loss')
-# plt.legend()
-# plt.show()
+loss, accuracy = model.evaluate(features_test, encoded_labels)
+print('Test Accuracy: %f' % (accuracy * 100))
+# 保存标签编码器
+with open('./sign-language-model.pkl', 'wb') as f:
+    pickle.dump(encoded_labels, f)
+
+# 绘制时间曲线图
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+# 绘制损失函数曲线
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
